4c98c239f60fe429a4765796b0542cd2203e1ed5
hyunugee <rychekor@gmail.com>
2026-02-04 16:07:25 +0900

    Fix: Disable PWA temporary

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    register: true,
-    skipWaiting: true,
-    workboxOptions: {
-        disableDevLogs: true,
-    },
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
@@ -18,4 +8,4 @@ const nextConfig = {
     },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
