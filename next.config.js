fc48201783bb91226fa0823e44485194c70a0a44
hyunugee <rychekor@gmail.com>
2026-02-04 15:49:38 +0900

    Fix: Switch to compatible PWA library

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
     dest: "public",
     disable: process.env.NODE_ENV === "development",
     register: true,
     skipWaiting: true,
+    workboxOptions: {
+        disableDevLogs: true,
+    },
 });
 
 /** @type {import('next').NextConfig} */
